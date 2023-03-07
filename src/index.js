04f79d73306975ce468d2df3f0e5aaa0e0afc55e
matthew <permahuman@gmail.com>
2023-03-06 21:09:25 -0800

    initial commit with full webpack configuration and barebones html and index.js hello world.

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './style.css'
+/* rememebr to import images here or elsehwere for use in your code*/
+
+
+
+
+
+const content = document.querySelector('#content')
+
+
+
+
+
+console.log("Hello world! My name is Uche clown.");
